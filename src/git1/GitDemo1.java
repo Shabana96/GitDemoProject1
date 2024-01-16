bc3adc0ec4266a01e83332907769112b13746aa8
sha94 <sha94@LAPTOP-ACKL0PJI>
2024-01-16 21:39:52 +0000

    FIRST COMMIT...

---
+++ src/git1/GitDemo1.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package git1;
+
+/**
+ * 
+ */
+public class GitDemo1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
